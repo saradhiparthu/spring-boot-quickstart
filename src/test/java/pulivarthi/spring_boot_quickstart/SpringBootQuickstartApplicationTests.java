0a21e50f1a15784979f7299779cbb8a4ae682ef3
saradhiparthu <saradhiparthu@gmail.com>
2024-12-10 22:00:50 -0500

    Initial commit

---
+++ src/test/java/pulivarthi/spring_boot_quickstart/SpringBootQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package pulivarthi.spring_boot_quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
