032fa48c9fdade0acd57499d96281010da9246dc
saradhiparthu <saradhiparthu@gmail.com>
2024-12-27 21:44:27 -0500

    updated version

--- src/main/java/quickstart/App.java
+++ src/main/java/quickstart/App.java
@@ -16,6 +16,6 @@ public class App {
 	
 	@GetMapping("/hello")
    	 public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      		return String.format("Hello pulivarthi %s!", name);
+      		return String.format("Hello Pulivarthi %s!", name);
 	}
 }
